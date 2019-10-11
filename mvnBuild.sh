c68c9430607763801cbd116351c7c911fa8d7bf3
Rémi Barat <rba@activeviam.com>
2019-10-11 10:55:35 +0200

    fix mvnBuild by moving repo to cdbashare

--- mvnBuild.sh
+++ mvnBuild.sh
@@ -16,8 +16,8 @@ export M2_HOME=/opt/cdba/maven  #$M2_HOME #"$BASEDIR/../maven"
 export M2=$M2_HOME/bin
 export PATH="$M2:$PATH"
 export ACTIVEPIVOT_LICENSE=/cdbashare/ActivePivot.lic.21127 #"$BASEDIR/../license"
-export MVN_REPO=/home_nfs/home_guimezag/maven/repo #"$BASEDIR/../maven_repository"
-export MVN_SETTINGS=/home_nfs/home_guimezag/maven/settings.xml
+export MVN_REPO=/cdbashare/activeviam/maven/repo #"$BASEDIR/../maven_repository"
+export MVN_SETTINGS=/cdbashare/activeviam/maven/settings.xml
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0 #"$BASEDIR/../java"
 export HOSTNAME=`hostname` #"quartet1.atc-hp.com"
 
@@ -25,7 +25,7 @@ export HOSTNAME=`hostname` #"quartet1.atc-hp.com"
 
 echo "Recompiling project"
 #mvn -s $MVN_SETTINGS test # Enough for having the classes in target
-mvn -s $MVN_SETTINGS install
+mvn -s $MVN_SETTINGS -o install
 
 
 
